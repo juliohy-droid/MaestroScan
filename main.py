e2e7a59d32013843c64ed7e1296136a55a0f53f3
juliohy-droid <juliohy@gmail.com>
2025-12-28 15:48:25 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import flet as ft
 from app.gui_mobile import main
 
 if __name__ == "__main__":
-    # Render asigna el puerto mediante la variable PORT
-    # Si no existe, usamos 8080 por defecto
-    port = int(os.getenv("PORT", 8080))
+    # Render asigna el puerto 10000 o mediante la variable PORT
+    port = int(os.getenv("PORT", 10000))
     
-    # Configuramos para que Flet corra como servidor web puro
     ft.app(
         target=main,
         view=ft.AppView.WEB_BROWSER,
         port=port,
-        host="0.0.0.0"  # Esto es lo que abre la app al mundo
+        host="0.0.0.0"
     )
